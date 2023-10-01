8166f3cb140071ad441f1655924e7b8b987345a7
anton.bashirov <anton.sh.local@gmail.com>
2023-10-02 01:37:30 +0300

    tests

---
+++ test/lease.dart
@@ -0,0 +1,1 @@
+void lease() {}
--- test/test.dart
+++ test/test.dart
@@ -1,9 +1,11 @@
 import 'errors.dart';
 import 'fragmentation.dart';
 import 'interaction.dart';
+import 'lease.dart';
 
 void main() {
   interaction();
   fragmentation();
+  lease();
   errors();
 }
