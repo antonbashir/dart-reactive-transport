9045a0a00d586093c54ffeaf83be5cc411c13d20
anton.bashirov <anton.sh.local@gmail.com>
2023-10-02 01:57:05 +0300

    tests

--- test/test.dart
+++ test/test.dart
@@ -2,12 +2,14 @@ import 'backpressure.dart';
 import 'errors.dart';
 import 'fragmentation.dart';
 import 'interaction.dart';
+import 'keepalive.dart';
 import 'lease.dart';
 
 void main() {
   interaction();
   backpressure();
   fragmentation();
+  keepalive();
   lease();
   errors();
 }
