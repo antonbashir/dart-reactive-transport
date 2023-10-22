ff9628e452b1105ed1e3cb4bd5ef47a218c08cde
anton.bashirov <anton.sh.local@gmail.com>
2023-10-23 00:24:48 +0300

    tests

--- test/fragmentation.dart
+++ test/fragmentation.dart
@@ -49,4 +49,51 @@ void fragmentation() {
 
     await reactive.shutdown(transport: true);
   });
+
+  test('1 simple request + 1 fragmented request', timeout: Timeout.none, () async {
+    final transport = Transport();
+    final worker = TransportWorker(transport.worker(ReactiveTransportDefaults.transport().workerConfiguration));
+    await worker.initialize();
+    final reactive = ReactiveTransport(transport, worker, ReactiveTransportDefaults.transport());
+    final simplePayload = Uint8List.fromList("simple".codeUnits);
+    final fullPayload = Uint8List.fromList(List.generate(1 * 1024 * 1024, (index) => 31));
+
+    final latch = Latch(2);
+
+    reactive.serve(
+      InternetAddress.anyIPv4,
+      12345,
+      (connection) => connection.subscriber.subscribe(
+        "channel",
+        onSubscribe: (producer) => producer.request(2),
+        onPayload: (payload, producer) {
+          if (latch.count == 0) {
+            expect(ListEquality().equals(payload, simplePayload), true);
+            latch.notify();
+            return;
+          }
+          expect(ListEquality().equals(payload, fullPayload), true);
+          latch.notify();
+        },
+      ),
+    );
+
+    reactive.connect(
+      InternetAddress.loopbackIPv4,
+      12345,
+      setupConfiguration: ReactiveTransportDefaults.setup().copyWith(dataMimeType: octetStreamMimeType),
+      (connection) => connection.subscriber.subscribe(
+        "channel",
+        configuration: ReactiveTransportDefaults.channel().copyWith(frameMaxSize: 1024, fragmentSize: 256, chunksLimit: 2),
+        onRequest: (count, producer) {
+          producer.payload(simplePayload);
+          producer.payload(fullPayload);
+        },
+      ),
+    );
+
+    await latch.done();
+
+    await reactive.shutdown(transport: true);
+  });
 }
