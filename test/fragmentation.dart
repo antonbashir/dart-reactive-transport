4e1de0b2b9e44de9ee6b0e54c83ace13ac4af96e
anton.bashirov <anton.sh.local@gmail.com>
2023-11-13 00:21:33 +0200

    tests

--- test/fragmentation.dart
+++ test/fragmentation.dart
@@ -150,4 +150,65 @@ void fragmentation() {
 
     await reactive.shutdown(transport: true);
   });
+
+  test('1 simple request -> 1 fragmented request -> 1 simple request -> 1 fragmented request', timeout: Timeout.none, () async {
+    final transport = Transport();
+    final worker = TransportWorker(transport.worker(ReactiveTransportDefaults.transport().workerConfiguration));
+    await worker.initialize();
+    final reactive = ReactiveTransport(transport, worker, ReactiveTransportDefaults.transport());
+    final simplePayload = Uint8List.fromList("simple".codeUnits);
+    final fullPayload = Uint8List.fromList(List.generate(1 * 1024 * 1024, (index) => 31));
+
+    final latch = Latch(4);
+
+    reactive.serve(
+      InternetAddress.anyIPv4,
+      12345,
+      (connection) => connection.subscriber.subscribe(
+        "channel",
+        onSubscribe: (producer) => producer.request(4),
+        onPayload: (payload, producer) {
+          if (latch.count == 0) {
+            expect(ListEquality().equals(payload, simplePayload), true);
+            latch.notify();
+            return;
+          }
+          if (latch.count == 1) {
+            expect(ListEquality().equals(payload, fullPayload), true);
+            latch.notify();
+            return;
+          }
+          if (latch.count == 2) {
+            expect(ListEquality().equals(payload, simplePayload), true);
+            latch.notify();
+            return;
+          }
+          expect(ListEquality().equals(payload, fullPayload), true);
+          latch.notify();
+          return;
+        },
+      ),
+    );
+
+    reactive.connect(
+      InternetAddress.loopbackIPv4,
+      12345,
+      setupConfiguration: ReactiveTransportDefaults.setup().copyWith(dataMimeType: octetStreamMimeType),
+      (connection) => connection.subscriber.subscribe(
+        "channel",
+        configuration: ReactiveTransportDefaults.channel().copyWith(frameMaxSize: 1024, fragmentSize: 256, chunksLimit: 2),
+        onError: (code, error, producer) => print(error),
+        onRequest: (count, producer) {
+          producer.payload(simplePayload);
+          producer.payload(fullPayload);
+          producer.payload(simplePayload);
+          producer.payload(fullPayload);
+        },
+      ),
+    );
+
+    await latch.done();
+
+    await reactive.shutdown(transport: true);
+  });
 }
