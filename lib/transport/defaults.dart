e2b40171a16bf399488978ef09a3405265baaf6d
anton.bashirov <anton.sh.local@gmail.com>
2024-01-13 19:59:07 +0200

    docs

--- lib/transport/defaults.dart
+++ lib/transport/defaults.dart
@@ -16,8 +16,8 @@ class ReactiveTransportDefaults {
   static ReactiveChannelConfiguration channel() => ReactiveChannelConfiguration(
         initialRequestCount: 1,
         chunksLimit: 8,
-        fragmentSize: 5 * 1024 * 1024,
-        frameMaxSize: 10 * 1024 * 1024,
+        frameMaxSize: 5 * 1024 * 1024,
+        fragmentSize: 10 * 1024 * 1024,
       );
 
   static ReactiveBrokerConfiguration broker() => ReactiveBrokerConfiguration(
